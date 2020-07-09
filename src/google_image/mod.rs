eca028cfdae6165b73dbfdccc8d2c4d6f369908a
Minghao, Li <leemiyinghao@gmx.com>
2020-07-09 19:00:19 +0800

    large image

--- src/google_image/mod.rs
+++ src/google_image/mod.rs
@@ -1,10 +1,12 @@
-use hyper::{Body, Client, Request};
+use hyper::{Body, Client, Request, Uri};
 extern crate hyper_tls;
 use hyper_tls::HttpsConnector;
 use percent_encoding::{utf8_percent_encode, AsciiSet, CONTROLS};
 use scraper::{Html, Selector};
 use serde::Deserialize;
 use serde_json;
+use std::fs::File;
+use std::io::Write;
 use tokio::runtime::Runtime;
 
 #[derive(Debug)]
@@ -27,48 +29,75 @@ async fn search(keyword: &str) -> Result<ImageTarget, String> {
     const FRAGMENT: &AsciiSet = &CONTROLS.add(b' ').add(b'"').add(b'<').add(b'>').add(b'`');
     let https = HttpsConnector::new();
     let client = Client::builder().build::<_, hyper::Body>(https);
-    let uri = format!("https://www.google.com/search?q={}&espv=2&biw=1920&bih=966&site=webhp&source=lnms&tbm=isch&sa=X&ei=XosDVaCXD8TasATItgE&ved=0CAcQ_AUoAg", utf8_percent_encode(keyword, FRAGMENT).collect::<String>()).parse();
-    if uri.is_err() {
+    let uri = format!("https://www.google.com/search?q={}&espv=2&biw=1920&bih=966&site=webhp&source=lnms&tbm=isch&sa=X&ei=XosDVaCXD8TasATItgE&ved=0CAcQ_AUoAg", utf8_percent_encode(keyword, FRAGMENT).collect::<String>());
+    let mut request = Request::get(uri)
+        .header(
+            "Accept",
+            "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        )
+        .header(
+            "User-Agent",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0",
+        )
+        .body(Body::from(bytes::Bytes::new()));
+    if request.is_err() {
         return Err(String::from("uri parse fail"));
     }
-    let page = client.get(uri.unwrap()).await;
+    let page = client.request(request.unwrap()).await;
     if page.is_err() {
         return Err(String::from("page fetch fail"));
     }
     let buf = hyper::body::to_bytes(page.unwrap()).await;
     if buf.is_err() {
+        println!("{:?}", buf);
         return Err(String::from("body to bytes fail"));
     }
     let buff = buf.unwrap();
+    // let mut output = File::create("result.html").unwrap();
+    // output.write(&buff).unwrap();
     let page = String::from_utf8_lossy(&buff);
-    let document = Html::parse_document(&page[..]);
-    let selector = Selector::parse(r#"img.t0fcAb"#).unwrap(); //no need to check static variable
-    let raw_jsdata = document.select(&selector).next();
-    if raw_jsdata.is_none() {
-        return Err(String::from("body to bytes fail"));
+    let target_rule = regex::Regex::new(r#"\["GRID_STATE0".+"(?:https?[^"]+)"[^\]]+\][^\[]+\["(https?[^"]+)""#).unwrap();
+    // let document = Html::parse_document(&page[..]);
+    // let selector = Selector::parse(r#"img.t0fcAb"#).unwrap(); //no need to check static variable
+    // let raw_jsdata = document.select(&selector).next();
+    // if raw_jsdata.is_none() {
+    //     println!("{:?}", raw_jsdata);
+    //     return Err(String::from("raw_jsdata not found"));
+    // }
+    // let raw_jsdata_element = raw_jsdata.unwrap();
+    // let img_url = raw_jsdata_element.value().attr("src");
+    // let page_url = match raw_jsdata_element.parent() {
+    //     Some(x) => match x.parent() {
+    //         Some(y) => match scraper::ElementRef::wrap(y) {
+    //             Some(z) => match z.value().attr("href") {
+    //                 Some(xx) => Some(xx),
+    //                 None => None,
+    //             },
+    //             None => None,
+    //         },
+    //         None => None,
+    //     },
+    //     None => None,
+    // };
+    // if img_url.is_none() || page_url.is_none() {
+    //     return Err(String::from("urls non-exist"));
+    // }
+    // Ok(ImageTarget {
+    //     img_url: String::from(img_url.unwrap()),
+    //     page_url: String::from(&page_url.unwrap()[7..]),
+    // })
+    let m = target_rule.captures(&page[..]);
+    if m.is_none(){
+        return Err(String::from("img_url not found"));
     }
-    let raw_jsdata_element = raw_jsdata.unwrap();
-    let img_url = raw_jsdata_element.value().attr("src");
-    let page_url = match raw_jsdata_element.parent() {
-        Some(x) => match x.parent() {
-            Some(y) => match scraper::ElementRef::wrap(y) {
-                Some(z) => match z.value().attr("href") {
-                    Some(xx) => Some(xx),
-                    None => None,
-                },
-                None => None,
-            },
-            None => None,
-        },
-        None => None,
-    };
-    if img_url.is_none() || page_url.is_none() {
-        return Err(String::from("urls non-exist"));
+    let img_url = m.unwrap().get(1);
+    match img_url {
+        Some(x) => Ok(ImageTarget {
+            img_url: String::from(x.as_str()),
+            page_url: String::from(x.as_str()),
+        }),
+        None => Err(String::from("img_url not found")),
     }
-    Ok(ImageTarget {
-        img_url: String::from(img_url.unwrap()),
-        page_url: String::from(page_url.unwrap()),
-    })
 }
 async fn download(url: String) -> Result<Box<bytes::Bytes>, String> {
     let https = HttpsConnector::new();
